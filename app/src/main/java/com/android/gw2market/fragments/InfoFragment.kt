ef32f5a34f5cbc96017fcfd221b42107a34d53a6
FernandoH-G <fherrera2018@gmail.com>
2020-08-19 00:14:16 -0700

    Added Volley for a simple GET request in Android
    Was able to test the sample GW2 api to test if volley returned a
    response. It does!

--- app/src/main/java/com/android/gw2market/fragments/InfoFragment.kt
+++ app/src/main/java/com/android/gw2market/fragments/InfoFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.android.gw2market.R
 import com.android.gw2market.databinding.FragmentInfoBinding
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class InfoFragment : Fragment() {
     private var tmpBinding: FragmentInfoBinding? = null
@@ -18,12 +22,20 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        tmpBinding = FragmentInfoBinding.inflate(inflater,container,false)
+        tmpBinding = FragmentInfoBinding.inflate(inflater, container, false)
         return iBinding.root
     }
 
     override fun onResume() {
         super.onResume()
+        val queue = Volley.newRequestQueue(requireContext())
+        // url needs to be reworked.
+        val url = "http://api.gw2tp.com/1/items?ids=123,345&fields=name,sell"
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> {  resp ->
+               iBinding.TXTVItemInfo.text = "response is: $resp"
+            }, Response.ErrorListener { iBinding.TXTVItemInfo.text = "Something wrong!"})
+        queue.add(stringRequest)
     }
 
     override fun onDestroyView() {
