572ba903c63e170b28e3494dff4fb94fb77f65ed
FernandoH-G <fherrera2018@gmail.com>
2020-08-20 17:04:44 -0700

    App is able to display name, sell, and img of the queried item id
    Need to figure out how to deal with items not in the api DB. Maybe
    this wont be an issue once string search is enabled.

--- app/src/main/java/com/android/gw2market/fragments/InfoFragment.kt
+++ app/src/main/java/com/android/gw2market/fragments/InfoFragment.kt
@@ -1,18 +1,21 @@
 package com.android.gw2market.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
-import com.android.gw2market.R
 import com.android.gw2market.databinding.FragmentInfoBinding
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
+import java.io.InputStream
+import java.net.URL
 
 class InfoFragment : Fragment() {
     private var tmpBinding: FragmentInfoBinding? = null
@@ -33,13 +36,23 @@ class InfoFragment : Fragment() {
         val queue = Volley.newRequestQueue(requireContext())
         val itemIDArg : InfoFragmentArgs by navArgs()
         val itemID = itemIDArg.itemId
-        val url = "http://api.gw2tp.com/1/items?ids=${itemID}&fields=name,sell"
+        val url = "http://api.gw2tp.com/1/items?ids=${itemID}&fields=name,sell,img"
 
-        val stringRequest = StringRequest(Request.Method.GET, url,
-            Response.Listener<String> {  resp ->
-               iBinding.TXTVItemInfo.text = "$resp"
-            }, Response.ErrorListener { iBinding.TXTVItemInfo.text = "Something wrong!"})
-        queue.add(stringRequest)
+        val jsonRequest = JsonObjectRequest(Request.Method.GET,url, null,
+        Response.Listener { resp ->
+            val obj = resp.getJSONArray("results")
+//            Log.d("item is null?: ", obj.isNull(0).toString())
+            if (obj.isNull(0)) return@Listener
+            // Figure out an elegant way to deal with the item not being in the DB.
+            val itemName = obj.getJSONArray(0).get(1).toString()
+            val itemPrice = obj.getJSONArray(0).get(2).toString()
+            val itemImgUrl = obj.getJSONArray(0).get(3).toString()
+            Picasso.with(requireContext()).load(itemImgUrl).into(iBinding.IMGVItemIcon)
+            iBinding.TXTVItemInfo.text = itemName
+            iBinding.TXTVItemPrice.text = itemPrice
+        }, Response.ErrorListener { iBinding.TXTVItemInfo.text = "Something wrong with json" })
+
+        queue.add(jsonRequest)
     }
 
     override fun onDestroyView() {
